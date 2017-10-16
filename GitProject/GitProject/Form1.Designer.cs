a147d7f2c5eb29be3be1f65da17b55b4bf475541
Alex Michenko <samson1513@gmail.com>
2017-10-16 12:11:39 +0300

    Change text - add symbol

--- GitProject/GitProject/Form1.Designer.cs
+++ GitProject/GitProject/Form1.Designer.cs
@@ -38,7 +38,7 @@
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(62, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Hello World";
+            this.label1.Text = "Hello World!";
             // 
             // Form1
             // 
